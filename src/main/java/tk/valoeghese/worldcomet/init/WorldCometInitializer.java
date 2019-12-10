d984a03020821bad96a99ede55ccbb33e4eaa40a
valoeghese <valoeghesemods@gmail.com>
2019-12-10 20:22:47 +1300

    fix test package

--- src/main/java/tk/valoeghese/worldcomet/init/WorldCometInitializer.java
+++ src/main/java/tk/valoeghese/worldcomet/init/WorldCometInitializer.java
@@ -2,12 +2,11 @@ package tk.valoeghese.worldcomet.init;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
-import tk.valoeghese.worldcomettest.TestMod;
 
 public class WorldCometInitializer implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		TestMod.init();
+		// nothing here, for now
 	}
 	
 	public static final Identifier from(String id) {
---
+++ src/test/java/tk/valoeghese/worldcomettest/TestMod.java
@@ -0,0 +1,14 @@
+package tk.valoeghese.worldcomettest;
+
+import net.fabricmc.api.ModInitializer;
+import tk.valoeghese.worldcomet.impl.gen.WorldCometChunkGeneratorType;
+
+public class TestMod implements ModInitializer {
+	public static WorldCometChunkGeneratorType cgt;
+
+	@Override
+	public void onInitialize() {
+		// TODO Auto-generated method stub
+		
+	}
+}
