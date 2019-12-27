a41aeeb8655f337ea12b8bc018cf621c44d53ea4
valoeghese <valoeghesemods@gmail.com>
2019-12-27 23:47:52 +1300

    r

--- src/main/java/tk/valoeghese/worldcomet/api/surface/fractal/SurfaceProviderFractal.java
+++ src/main/java/tk/valoeghese/worldcomet/api/surface/fractal/SurfaceProviderFractal.java
@@ -3,6 +3,7 @@ package tk.valoeghese.worldcomet.api.surface.fractal;
 import java.util.function.LongFunction;
 
 import net.minecraft.world.biome.layer.type.InitLayer;
+import net.minecraft.world.biome.layer.type.MergingLayer;
 import net.minecraft.world.biome.layer.type.ParentedLayer;
 import net.minecraft.world.biome.layer.util.CachingLayerContext;
 import net.minecraft.world.biome.layer.util.CachingLayerSampler;
@@ -22,25 +23,29 @@ public class SurfaceProviderFractal {
 		private final FractalRandomWrapper frw = new FractalRandomWrapper();
 
 		private Builder(InitLayer init) {
-			this.function = seed -> init.create(this.frw.getRandom());
+			this.function = seed -> init.create(this.frw.getRandomSource(1L));
 		}
 
-		public Builder stack(ParentedLayer layer) {
-			this.function = seed -> layer.create(this.frw.getRandom(), this.function.apply(seed));
+		public Builder stack(ParentedLayer layer, long salt) {
+			this.function = seed -> layer.create(this.frw.getRandomSource(salt), this.function.apply(seed));
 			return this;
+		}
+
+		public Builder merge(MergingLayer layer, Builder other, long salt) {
+			this.function = seed -> layer.create(this.frw.getRandomSource(salt), layer1, layer2)
 		}
 	}
 }
 
 class FractalRandomWrapper {
-	private CachingLayerContext random;
+	private LongFunction<CachingLayerContext> randomSource;
 
-	public FractalRandomWrapper setRandom(CachingLayerContext random) {
-		this.random = random;
+	public FractalRandomWrapper setRandomSource(LongFunction<CachingLayerContext> random) {
+		this.randomSource = random;
 		return this;
 	}
 
-	public CachingLayerContext getRandom() {
-		return this.random;
+	public CachingLayerContext getRandomSource(long salt) {
+		return this.randomSource.apply(salt);
 	}
 }
