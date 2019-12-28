477ee5a4fa6b04d0ec55ed704e2773710c899799
valoeghese <valoeghesemods@gmail.com>
2019-12-28 22:20:51 +1300

    finish fractal helper stuff

--- src/main/java/tk/valoeghese/worldcomet/api/surface/fractal/FractalSurfaceProvider.java
+++ src/main/java/tk/valoeghese/worldcomet/api/surface/fractal/FractalSurfaceProvider.java
@@ -1,11 +1,11 @@
 package tk.valoeghese.worldcomet.api.surface.fractal;
 
-import java.util.Map;
 import java.util.function.IntFunction;
 import java.util.function.LongFunction;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.world.biome.layer.util.CachingLayerSampler;
-import net.minecraft.world.biome.layer.util.LayerFactory;
 import tk.valoeghese.worldcomet.api.surface.Surface;
 import tk.valoeghese.worldcomet.api.surface.SurfaceProvider;
 import tk.valoeghese.worldcomet.impl.VoronoiSurfaceAccess;
@@ -13,9 +13,20 @@ import tk.valoeghese.worldcomet.impl.VoronoiSurfaceAccess;
 public class FractalSurfaceProvider implements SurfaceProvider {
 	private final VoronoiSurfaceAccess voronoiAccess;
 
-	public FractalSurfaceProvider(long seed, FractalLayerProvider sample) {
+	/**
+	 * @apiNote This constructor can be either used directly or handled via the helper factory builder.
+	 * @param seed the world seed.
+	 * @param sample a function which provides the caching layer sampler for the specified height input.
+	 * @param surfaceIdMap object representing an id map between the integer output of the fractal sampler and surfaces.
+	 */
+	public FractalSurfaceProvider(long seed, FractalLayerProvider sampler, SurfaceIdMap surfaceIdMap) {
 		this.voronoiAccess = new VoronoiSurfaceAccess(this, seed);
+		this.sampler = sampler;
+		this.surfaceMappingFuntion = surfaceIdMap.getMappingFunction();
 	}
+	
+	private final FractalLayerProvider sampler;
+	private final IntFunction<Surface> surfaceMappingFuntion;
 
 	@Override
 	public Surface getSurface(int x, int z, int height) {
@@ -27,28 +38,50 @@ public class FractalSurfaceProvider implements SurfaceProvider {
 		return this.getFractalSurface(genX, genZ, genY << 3);
 	}
 
+	/**
+	 * @param defaultFractal the FractalLongFunction to fall back upon if the height input gives an unmapped lookup id.
+	 */
 	public static FactoryBuilder factoryBuilder(FractalLongFunction defaultFractal) {
 		return new FactoryBuilder(defaultFractal);
 	}
 
 	public Surface getFractalSurface(int genX, int genZ, int height) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.surfaceMappingFuntion.apply(this.sampler.apply(height).sample(genX, genZ));
 	}
 
 	public static class FactoryBuilder {
-		final FractalLongFunction defaultFractal;
-		final Map<String, FractalLongFunction> functionNameMap = new HashMap<>();
-		//TODO layerNameMap. these two maps allow creating height layer functions easily
+		private final FractalLongFunction defaultFractal;
+		private final Int2ObjectMap<FractalLongFunction> functionIdMap = new Int2ObjectArrayMap<>();
 
 		private FactoryBuilder(FractalLongFunction defaultFractal) {
+			this.defaultFractal = defaultFractal;
 		}
 
-		public LongFunction<FractalSurfaceProvider> buildFactory() {
-			return seed -> new FractalSurfaceProvider(seed);
+		public FactoryBuilder addFactory(int lookupId, FractalLongFunction fractal) {
+			this.functionIdMap.put(lookupId, fractal);
+			return this;
+		}
+
+		public LongFunction<FractalSurfaceProvider> buildFactory(Height2FractalFunction heightToFractalFunction, SurfaceIdMap surfaceIdMap) {
+			return seed -> {
+				Int2ObjectMap<CachingLayerSampler> samplerIdMap = new Int2ObjectArrayMap<>();
+				CachingLayerSampler defaultSampler = this.defaultFractal.apply(seed).make();
+
+				this.functionIdMap.forEach((lookupId, setupFunction) -> {
+					samplerIdMap.put((int) lookupId, setupFunction.apply(seed).make());
+				});
+
+				return new FractalSurfaceProvider(seed, height -> {
+					return samplerIdMap.getOrDefault(heightToFractalFunction.getFractalLookupId(height), defaultSampler);
+				}, surfaceIdMap);
+			};
 		}
 	}
 
-	public static interface FractalLayerProvider extends IntFunction<LayerFactory<CachingLayerSampler>> {
+	public static interface FractalLayerProvider extends IntFunction<CachingLayerSampler> {
+	}
+	
+	public static interface Height2FractalFunction {
+		int getFractalLookupId(int height);
 	}
 }
