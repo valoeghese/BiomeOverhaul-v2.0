8247f4ff5fe34bc4e418223f7281ad87660c7ae3
valoeghese <valoeghesemods@gmail.com>
2020-03-16 13:05:57 +1300

    more documentation

--- src/main/java/tk/valoeghese/worldcomet/api/surface/fractal/FractalLongFunction.java
+++ src/main/java/tk/valoeghese/worldcomet/api/surface/fractal/FractalLongFunction.java
@@ -12,25 +12,45 @@ import net.minecraft.world.biome.layer.util.CachingLayerContext;
 import net.minecraft.world.biome.layer.util.CachingLayerSampler;
 import net.minecraft.world.biome.layer.util.LayerFactory;
 
+/**
+ * Interface with a {@link FractalLongFunction.Builder builder} which takes the world seed and returns a {@link LayerFactory layer factory} for {@link tk.valoeghese.api.surface.Surface surface} ids.
+ * Which id corresponds to which surface is determined by the particular world generator, typically through a {@link SurfaceIdMap}.
+ */
+@FunctionalInterface
 public interface FractalLongFunction extends LongFunction<LayerFactory<CachingLayerSampler>> {
+	/**
+	 * Creates a builder with the specified {@link InitLayer} as the initial layer to start the fractal generation.
+	 */
 	public static Builder builder(InitLayer init) {
 		return new Builder(init);
 	}
 
 	public static class Builder {
-		private FractalLongFunction function;
-		private final FractalRandomWrapper frw = new FractalRandomWrapper();
-
 		private Builder(InitLayer init) {
 			this.function = seed -> init.create(this.frw.getRandomSource(1L));
 		}
 
+		private FractalLongFunction function;
+		private final FractalRandomWrapper frw = new FractalRandomWrapper();
+
+		/**
+		 * Stacks the specified ParentedLayer transformer on the fractal generator.
+		 * 
+		 * @param layer the {@link ParentedLayer} (fractal modification layer which takes a sampler of the previous layer, x, z, random, and returns the processed result).
+		 * @param salt a number which is combined with the world seed to create a unique seed for the pseudorandom number generator.
+		 */
 		public Builder stack(ParentedLayer layer, long salt) {
 			final FractalLongFunction previous = this.function;
 			this.function = seed -> layer.create(this.frw.getRandomSource(salt), previous.apply(seed));
 			return this;
 		}
 
+		/**
+		 * Stacks the specified ParentedLayer transformer on the fractal generator.
+		 * 
+		 * @param layer the {@link SouthEastSamplingLayer} (fractal modification layer which takes the previous layer's int value and a pseudorandom number generator and returns the processed result).
+		 * @param salt a number which is combined with the world seed to create a unique seed for the pseudorandom number generator.
+		 */
 		public Builder stackDirectSampling(SouthEastSamplingLayer layer, long salt) {
 			return this.stack(layer, salt);
 		}
@@ -45,6 +65,12 @@ public interface FractalLongFunction extends LongFunction<LayerFactory<CachingLa
 			return this;
 		}
 
+		/**
+		 * Scales the fractal up (zooms in) the number of times specified in the count parameter. Each zoom magnifies 2x while smoothing and adding randomness to the edges.
+		 * 
+		 * @param initialSalt the initial salt value. The salt is a number which is combined with the world seed to create a unique seed for the pseudorandom number generator.
+		 * @param count the number of times to perform a zoom.
+		 */
 		public Builder scale(long initialSalt, int count) {
 			for (int i = 0; i < count; ++i) {
 				this.stack(ScaleLayer.NORMAL, initialSalt + (long) i);
@@ -53,6 +79,11 @@ public interface FractalLongFunction extends LongFunction<LayerFactory<CachingLa
 			return this;
 		}
 
+		/**
+		 * Build an {@link FractalLongFunction}.
+		 * @param cacheCapacity the capacity of the cache of values.
+		 * @return the built {@link FractalLongFunction}.
+		 */
 		public FractalLongFunction build(int cacheCapacity) {
 			final FractalLongFunction previous = this.function;
 			this.function = seed -> {
@@ -62,6 +93,10 @@ public interface FractalLongFunction extends LongFunction<LayerFactory<CachingLa
 			return this.function;
 		}
 
+		/**
+		 * Build an {@link FractalLongFunction}.
+		 * @return the built {@link FractalLongFunction}.
+		 */
 		public FractalLongFunction build() {
 			return this.build(25);
 		}
